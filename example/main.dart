b9c71a80327d1e5b70f2b0a6cc40e5593d1bbc74
MasahideMori-SimpleAppli <59500584+MasahideMori-SimpleAppli@users.noreply.github.com>
2025-12-06 20:53:34 +0900

    v0.0.2.

---
+++ example/main.dart
@@ -0,0 +1,4 @@
+void main() {
+  /// This package does not use any Dart code, so see the README for
+  /// instructions on how to set it up.
+}
---
+++ lib/simple_lint.dart
@@ -0,0 +1,4 @@
+/// This package provides a simple lint for Flutter
+/// that warns you about dangerous code.
+/// Note that you configure this package in yaml, not in Dart code.
+library;
